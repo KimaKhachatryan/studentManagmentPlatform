b2bdf21d7bb1a0c4fd94b3f855566100e97f783c
Rud Tadevosyan <rudtadevosyan07@mail.ru>
2025-10-01 19:34:38 +0400

    Added Specification folder for future work

---
+++ studentManagmentPlatform.Core/Specifications/Specification.cs
@@ -0,0 +1,9 @@
+﻿using System.Linq.Expressions;
+
+namespace studentManagmentPlatform.Core.Specifications;
+
+public abstract class Specification<T>
+{
+    public abstract Expression<Func<T, bool>> ToExpression();
+    
+}
