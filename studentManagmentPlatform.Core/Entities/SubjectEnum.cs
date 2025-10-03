fe6602882949afedef9a72dcf238e38009921ab4
User <User@DESKTOP-9U4TDQU>
2025-10-03 07:38:39 -0700

    fixes

--- studentManagmentPlatform.Core/Entities/SubjectEnum.cs
+++ studentManagmentPlatform.Core/Entities/SubjectEnum.cs
@@ -2,11 +2,12 @@
 
 public enum SubjectEnum
 {
-    Math,
-    Science,
-    History,
-    Literature,
-    Physics,
-    Chemistry,
-    Biology
+    Math = 0,
+    Science = 1,
+    History = 2,
+    Literature = 3,
+    Physics = 4,
+    Chemistry = 5,
+    Biology = 6
 }
+
