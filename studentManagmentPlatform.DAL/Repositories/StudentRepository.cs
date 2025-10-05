1e3c2d533674d95b4e7eb69595d6136317024de4
Lusine Tsutsulyan <lusinetsutsulyan20@gmail.com>
2025-10-05 13:25:12 +0400

    change CRUD operations

---
+++ studentManagmentPlatform.DAL/Repositories/StudentRepository.cs
@@ -0,0 +1,66 @@
+﻿using studentManagmentPlatform.Core.Entities;
+using studentManagmentPlatform.Core.Interfaces.RepositoryInterfaces;
+
+namespace studentManagmentPlatform.DAL
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly StudentDbContext _context;
+
+        public StudentRepository(StudentDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<Student?> GetById(int id)
+        {
+            var student = _context.Students.Find(id);
+            return Task.FromResult(student);
+        }
+
+        public Task<IEnumerable<Student>> GetAll()
+        {
+            var students = _context.Students.ToList();
+            return Task.FromResult<IEnumerable<Student>>(students);
+        }
+
+        public Task Add(Student entity)
+        {
+            _context.Students.Add(entity);
+            _context.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task Update(Student entity)
+        {
+            _context.Students.Update(entity);
+            _context.SaveChanges();
+            return Task.CompletedTask;
+        }
+
+        public Task Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if (student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+            return Task.CompletedTask;
+        }
+
+        public Task<Student?> GetByEmail(string email)
+        {
+            var student = _context.Students.FirstOrDefault(s => s.Email == email);
+            return Task.FromResult(student);
+        }
+
+        public Task<IEnumerable<Student>> GetByClassroom(int classroomId)
+        {
+            var students = _context.Students
+                .Where(s => s.ClassroomId == classroomId)
+                .ToList();
+            return Task.FromResult<IEnumerable<Student>>(students);
+        }
+    }
+
--- studentManagmentPlatform.Service/StudentService.cs
+++ studentManagmentPlatform.Service/StudentService.cs
@@ -1,90 +1,71 @@
-﻿namespace studentManagmentPlatform.Service
+﻿using studentManagmentPlatform.Core.Entities;
+using studentManagmentPlatform.Core.Interfaces.RepositoryInterfaces;
+using studentManagmentPlatform.Core.Interfaces.ServiceInterfaces;
+
+namespace studentManagmentPlatform.Service
 {
     public class StudentService : IStudentService
     {
         private readonly IStudentRepository _studentRepo;
-        private readonly IGradeRepository _gradeRepo;
-        private readonly ICourseRepository _courseRepo;
 
-        public StudentService(IStudentRepository studentRepo, IGradeRepository gradeRepo, ICourseRepository courseRepo)
+        public StudentService(IStudentRepository studentRepo)
         {
             _studentRepo = studentRepo;
-            _gradeRepo = gradeRepo;
-            _courseRepo = courseRepo;
         }
 
-        // Student Management
-        public void AddStudent(StudentDto student)
+        public Task<Student> DeleteStudent(int studentId)
         {
-            _studentRepo.Add(student);
-        }
-
-        public void UpdateStudent(int studentId, StudentDto student)
-        {
-            student.Id = studentId;
-            _studentRepo.Update(student);
-        }
-
-        public void DeleteStudent(int studentId)
-        {
+            var student = GetById(studentId);
             _studentRepo.Delete(studentId);
+            return student;
         }
 
-        public StudentDto GetStudentById(int studentId)
+        public Task<Student> GetById(int id)
         {
-            return _studentRepo.GetById(studentId);
+            var student = _studentRepo.GetById(id);
+            return student;
+        }
+        public Task<Student> RegisterStudent(Student student)
+        {
+            if (student == null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
+            _studentRepo.Add(student);
+            return Task.FromResult(student);
         }
 
-        public IEnumerable<StudentDto> GetAllStudents()
+        public Task UpdateStudent(Student student)
         {
-            return _studentRepo.GetAll();
+            if (student == null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
+            _studentRepo.Update(student);
+            return Task.FromResult(student);
         }
 
-        // Grade Management
-        public void AssignGrade(int studentId, int courseId, GradeDto grade)
+        Task IStudentService.DeleteStudent(int id)
         {
-            grade.StudentId = studentId;
-            grade.CourseId = courseId;
-            _gradeRepo.Add(grade);
+            var student = GetById(id);
+            if (student == null)
+            {
+                throw new ArgumentNullException(nameof(student));
+            }
+            _studentRepo.Delete(id);
+            return Task.FromResult(student);
         }
 
-        public void UpdateGrade(int gradeId, GradeDto grade)
+        public Task<IEnumerable<Student>> GetByClassroom(int classroomId)
         {
-            grade.Id = gradeId;
-            _gradeRepo.Update(grade);
-        }
+            var classroom = GetByClassroom(classroomId);
+            if (classroom == null)
+            {
+                throw new ArgumentNullException(nameof(classroom));
+            }
 
-        public void DeleteGrade(int gradeId)
-        {
-            _gradeRepo.Delete(gradeId);
-        }
-
-        public IEnumerable<GradeDto> GetGradesByStudentId(int studentId)
-        {
-            return _gradeRepo.GetByStudentId(studentId);
-        }
-
-        public double GetAverageGrade(int studentId)
-        {
-            var grades = _gradeRepo.GetByStudentId(studentId);
-            return grades.Any() ? grades.Average(g => g.Value) : 0;
-        }
-
-        // Course Enrollment
-        public void EnrollStudentInCourse(int studentId, int courseId)
-        {
-            _courseRepo.Enroll(studentId, courseId);
-        }
-
-        public void UnenrollStudentFromCourse(int studentId, int courseId)
-        {
-            _courseRepo.Unenroll(studentId, courseId);
-        }
-
-        public IEnumerable<CourseDto> GetCoursesByStudentId(int studentId)
-        {
-            return _courseRepo.GetByStudentId(studentId);
+            var students = _studentRepo.GetByClassroom(classroomId);
+            return students;
         }
     }
-
 }
